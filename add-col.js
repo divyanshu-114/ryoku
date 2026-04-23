36c4908f0ed76e35021c78822347b1b2349f8758
Trishix <trishitofficial@gmail.com>
2026-04-24 02:22:36 +0530

    chore: remove unnecessary files and scripts

--- add-col.js
+++
@@ -1,9 +0,0 @@
-const { neon } = require('@neondatabase/serverless');
-
-async function run() {
-  const sql = neon('postgresql://neondb_owner:npg_QNeoPydEZ5T3@ep-muddy-rice-a4dj6ujd-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require');
-  await sql`ALTER TABLE conversations ADD COLUMN IF NOT EXISTS customer_phone text;`;
-  console.log('Column added');
-}
-
-run().catch(console.error);
--- test-slug.mjs
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-const sql = neon(process.env.DATABASE_URL);
-
-async function main() {
-  const res = await sql`SELECT slug FROM businesses LIMIT 1`;
-  console.log(res);
-}
-
-main();
